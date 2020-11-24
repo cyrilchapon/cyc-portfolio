e61e11161219aa6546a805ac64c6b8ef781f2d38
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2020-11-24 11:22:08 +0100

    Add SSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,33 @@
+import React from 'react'
 import { linksGenerator } from '$components/html-head'
 import { themes } from '$styles'
 import { CssBaseline, NoSsr, ThemeProvider } from '@material-ui/core'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { SERVER_STYLESHEET_ID } from './_document'
 
 // import { GlobalStyles, StyledNormalize, theme } from '$styles'
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      {linksGenerator()}
-    </Head>
+const App = ({ Component, pageProps }: AppProps) => {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector(`#${SERVER_STYLESHEET_ID}`)
+    jssStyles?.parentElement?.removeChild(jssStyles)
+  }, [])
 
-    <NoSsr>
+  return (
+    <>
+      <Head>
+        {linksGenerator()}
+      </Head>
+
       <ThemeProvider theme={themes.root}>
         <CssBaseline />
 
         <Component {...pageProps} />
       </ThemeProvider>
-    </NoSsr>
-  </>
-)
+    </>
+  )
+}
 
 export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,65 @@
+import React from 'react'
+import { ServerStyleSheets } from '@material-ui/core'
 import _Document, {
   Html,
   Head,
   Main,
   NextScript,
-  // DocumentContext,
-  DocumentInitialProps
+  DocumentInitialProps,
+  DocumentContext
 } from 'next/document'
 
-class Document extends _Document<DocumentInitialProps> {
+export const SERVER_STYLESHEET_ID = 'jss-serverside'
+
+class Document extends _Document {
+  static async getInitialProps (ctx: DocumentContext) {
+    // Resolution order
+    //
+    // On the server:
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. document.getInitialProps
+    // 4. app.render
+    // 5. page.render
+    // 6. document.render
+    //
+    // On the server with error:
+    // 1. document.getInitialProps
+    // 2. app.render
+    // 3. page.render
+    // 4. document.render
+    //
+    // On the client
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. app.render
+    // 4. page.render
+
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets({
+      id: SERVER_STYLESHEET_ID
+    })
+
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () => (
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      })
+    )
+
+    const initialProps = await _Document.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement()
+      ]
+    }
+  }
+
   render() {
     return (
       <Html>
